51e6cc443b63b7008ffe0cf0c715dc2724f85a7b
nikhilesh2010 <nikhilesh.cc@gmail.com>
2024-02-06 00:33:49 +0530

    added event handler

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={ <Home events={events}/> }/>
+        <Route path='/' element={ <Home events={events} addEventHandler={addEventHandler} /> }/>
       </Routes>
     </BrowserRouter>
   );
